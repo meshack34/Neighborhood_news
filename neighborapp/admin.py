d6d359a9c1d560118cdb180a11ff289c88931e86
kimutaimeshack <meshack.kimutai@student.moringaschool.com>
2022-01-08 19:07:18 +0300

    register models in admin

--- neighborapp/admin.py
+++ neighborapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Business, NeighborHood, Post, Profile
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(NeighborHood)
+admin.site.register(Business)
